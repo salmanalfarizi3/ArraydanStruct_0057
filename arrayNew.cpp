ef062dc8f3972de84828578cacaf46f7b6673db7
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-04-06 17:21:12 +0700

    Menampilkan data satu per satu

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -24,4 +24,9 @@ int main() {
     cout << "Data index 4 = ";
     cin >> kamu[4];
     cout << endl;
-  
+    // Menampilkan Satu per Satu
+    cout << "Data Pertama = " << kamu[0] << endl;
+    cout << "Data Kedua = " << kamu[1] << endl;
+    cout << "Data Ketiga = " << kamu[2] << endl;
+    cout << "Data Keempat = " << kamu[3] << endl;
+}
