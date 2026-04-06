7daa440725cebd97b851be2d44646ff124d4c487
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-04-06 17:48:36 +0700

    Menampilkan data Nama,Alamat dan umur

--- structNew.cpp
+++ structNew.cpp
@@ -18,4 +18,9 @@ int main() {
         cout << "Umur : ";
         cin >> mhs.umur;
         cout << endl;
- 
+        // tampilkan
+        cout << "Menampilkan Data" << endl;
+        cout << "Nama : " << mhs.nama << endl;
+        cout << "Alamat : " << mhs.alamat << endl;
+        cout << "Umur : " << mhs.umur << endl;
+    }
