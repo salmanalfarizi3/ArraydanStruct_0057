41a06689eba507a4198cd1b8fc81a0793f7fb0e7
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-04-06 18:38:07 +0700

    Menambahkan getline nama

--- getline.cpp
+++ getline.cpp
@@ -21,17 +21,4 @@ int main() {
         cout << "Mengisi Data" << endl;
         cout << "Nama : ";
         getline(cin, mhs.nama);
-        cout << "desa : ";
-        getline(cin, mhs.alamat.desa);
-        cout << "kota : ";
-        getline(cin, mhs.alamat.kota);
-        cout << "Umur : ";
-        cin >> mhs.umur;
-        cout << endl;
-        // tampilkan
-        cout << "Menampilkan Data" << endl;
-        cout << "Nama : " << mhs.nama << endl;
-        cout << "Desa : " << mhs.alamat.desa << endl;
-        cout << "Kota : " << mhs.alamat.kota << endl;
-        cout << "Umur : " << mhs.umur << endl;
-    }
+      
